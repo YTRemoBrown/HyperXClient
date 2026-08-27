2a50ffa081abdbc41aeb28ac471f7bc553f4cb46
YTRemoBrown <73560663+YTRemoBrown@users.noreply.github.com>
2026-08-27 19:28:32 +0300

    Load native library and update text display

--- app/src/main/java/com/hyperx/client/MainActivity.java
+++ app/src/main/java/com/hyperx/client/MainActivity.java
@@ -6,12 +6,18 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    static {
+        System.loadLibrary("HyperXClient");
+    }
+
+    public native String stringFromJNI();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         TextView text = new TextView(this);
-        text.setText("HyperX Client");
+        text.setText(stringFromJNI());
         text.setTextSize(28);
 
         setContentView(text);
